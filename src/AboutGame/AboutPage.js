5571f9315c81cf02181e2f2596658ce690bcc884
Geekster PC <8gordeladze8@gmail.com>
2024-01-29 20:55:27 +0400

    added better commends eddited costs

--- src/AboutGame/AboutPage.js
+++ src/AboutGame/AboutPage.js
@@ -7,6 +7,9 @@ const AboutPage = () => {
         <h3 style={{color: 'white'}}>თამაშში თქვენი მიზანია აკლიკოთ ცენტრში მოძრავ ღილაკს და შეაგროვოთ ქულები</h3>
         <h3 style={{color: 'white'}}>ამ ქულეით შეძლებთ upgrade-ბის შეძენას რაც დაგეხმარებათ მეტი ქულის შეგროვებაში</h3>
         <h3 style={{color: 'white'}}>თავიდან გამოჩნდება მხოლოდ ორი upgrade თუმცა როცა მათ იყიდით გამოჩნდება ახლებიც (10-ამდე)</h3>
+        <h3 style={{color: 'white'}}>პირველი upgrade ამატებს +1 ქულას ყოველ დაკლიკებაზე</h3>
+        <h3 style={{color: 'white'}}>მეორე upgrade autoclicker-ია და ამატებს ქულებს ავტომატურად 1.5 წამში</h3>
+        <h3 style={{color: 'white'}}>მესამე და შემდეგი upgrade-ები გავს მეორე upgrades თუმცა ამატებს უფრო მეტ ქულას</h3>
         <p style={{color: 'white'}}>თამაში დაწერილია React პლატფორმაზე და იყენებს json-server-ს</p>
 
         <img style={{height: '200px', width: '400px'}} src={`/background/react.gif`} alt="ReactImage"/>
--- src/upgrade_func/addingUpgrade.js
+++ src/upgrade_func/addingUpgrade.js
@@ -6,36 +6,36 @@ export async function  addUpgrade(object, setUpgrades) {
     console.log('object:', object);
   
     if (object.length === 2 && object[1].numberOfUpgrades >= 2){
-      setUpgrades([...object, {name: "python", numberOfUpgrades: 0, cost: 300}])
-      await apiService.postAddUpgrades({name: "python", numberOfUpgrades: 0, cost: 300});
+      setUpgrades([...object, {name: "python", numberOfUpgrades: 0, cost: 100}])
+      await apiService.postAddUpgrades({name: "python", numberOfUpgrades: 0, cost: 100});
     }
 
-    if (object.length === 3 && object[2].numberOfUpgrades >= 3){
-        setUpgrades([...object, {name: "frontend", numberOfUpgrades: 0, cost: 1000}])
-        await apiService.postAddUpgrades({name: "frontend", numberOfUpgrades: 0, cost: 1000});
+    if (object.length === 3 && object[2].numberOfUpgrades >= 2){
+        setUpgrades([...object, {name: "frontend", numberOfUpgrades: 0, cost: 300}])
+        await apiService.postAddUpgrades({name: "frontend", numberOfUpgrades: 0, cost: 300});
     }
 
-    if (object.length === 4 && object[3].numberOfUpgrades >= 5){
-        setUpgrades([...object, {name: "algorithms", numberOfUpgrades: 0, cost: 5000}])
-        await apiService.postAddUpgrades({name: "algorithms", numberOfUpgrades: 0, cost: 5000});
+    if (object.length === 4 && object[3].numberOfUpgrades >= 3){
+        setUpgrades([...object, {name: "algorithms", numberOfUpgrades: 0, cost: 1000}])
+        await apiService.postAddUpgrades({name: "algorithms", numberOfUpgrades: 0, cost: 1000});
     }
 
-    if (object.length === 5 && object[4].numberOfUpgrades >= 10){
-        setUpgrades([...object, {name: "c++", numberOfUpgrades: 0, cost: 10000}])
-        await apiService.postAddUpgrades({name: "c++", numberOfUpgrades: 0, cost: 10000});
+    if (object.length === 5 && object[4].numberOfUpgrades >= 3){
+        setUpgrades([...object, {name: "c++", numberOfUpgrades: 0, cost: 1500}])
+        await apiService.postAddUpgrades({name: "c++", numberOfUpgrades: 0, cost: 1500});
     }
 
-    if (object.length === 6 && object[5].numberOfUpgrades >= 20){
-        setUpgrades([...object, {name: "oop", numberOfUpgrades: 0, cost: 20000}])
-        await apiService.postAddUpgrades({name: "oop", numberOfUpgrades: 0, cost: 20000});
+    if (object.length === 6 && object[5].numberOfUpgrades >= 2){
+        setUpgrades([...object, {name: "oop", numberOfUpgrades: 0, cost: 10000}])
+        await apiService.postAddUpgrades({name: "oop", numberOfUpgrades: 0, cost: 10000});
     }
 
-    if (object.length === 7 && object[6].numberOfUpgrades >= 20){
-        setUpgrades([...object, {name: "java", numberOfUpgrades: 0, cost: 30000}])
-        await apiService.postAddUpgrades({name: "java", numberOfUpgrades: 0, cost: 30000});
+    if (object.length === 7 && object[6].numberOfUpgrades >= 5){
+        setUpgrades([...object, {name: "java", numberOfUpgrades: 0, cost: 20000}])
+        await apiService.postAddUpgrades({name: "java", numberOfUpgrades: 0, cost: 20000});
     }
     
-    if (object.length === 8 && object[7].numberOfUpgrades >= 10){
+    if (object.length === 8 && object[7].numberOfUpgrades >= 4){
         setUpgrades([...object, {name: "react", numberOfUpgrades: 0, cost: 50000}])
         await apiService.postAddUpgrades({name: "react", numberOfUpgrades: 0, cost: 50000});
     }
